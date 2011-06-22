b781fca0877f83b3e17b1de3dfcd694ee74c0f81
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-06-22 15:13:17 -0300

    Doing some testing functions (lena and circle)

--- binvol.py
+++ binvol.py
@@ -2,12 +2,18 @@ import mahotas
 import numpy as np
 from scipy import ndimage
 
-N = 10
+N = 100
+THRESHOLD = 0.0001
 
 def perim(image):
-    return mahotas.labeled.borders(image, np.ones((3, 3, 3)))
+    if image.ndim == 2:
+        # Connectivity 8
+        return mahotas.labeled.borders(image, np.ones((3, 3)))
+    elif image.ndim == 3:
+        # Connectivity 26
+        return mahotas.labeled.borders(image, np.ones((3, 3, 3)))
 
-def smooth(image):
+def smooth(image, n):
     dt = 1/6.0
     u = image.astype('float64')
     A0 = perim(image)
@@ -15,9 +21,10 @@ def smooth(image):
     A2 = perim(A1)
     A3 = perim(A2)
     A = A0 | A1 | A2 | A3
+    n = 0
 
     if image.ndim == 2:
-        for i in xrange(N):
+        while 1:
             gy, gx = np.gradient(u)
 
             # gradient magnitude
@@ -37,13 +44,23 @@ def smooth(image):
 
             nu = u.copy()
             temp_u = u + dt * H
-            nu[image] = np.fmax(temp_u[image], 0.5)
-            nu[~image] = np.fmin(temp_u[~image], 0.5)
+            nu[(image) & (A)] = np.fmax(temp_u[(image) & (A)], 0.5)
+            nu[(~image) & (A)] = np.fmin(temp_u[(~image) & (A)], 0.5)
+
+            avg_change = (1.0/np.sum(A) * np.sum((nu[A] - u[A])**2)) ** 0.5
+            print "avg", avg_change
+            #print n, "BinVol", np.sum(gx**2 + gy**2)
+            if avg_change <= THRESHOLD or n == N:
+                break
+            n += 1
 
             u = nu.copy()
 
+            del H
+            del temp_u
+
     elif image.ndim == 3:
-        for i in xrange(N):
+        while 1:
             gz, gy, gx = np.gradient(u)
 
             # gradient magnitude
@@ -65,10 +82,19 @@ def smooth(image):
 
             nu = u.copy()
             temp_u = u + dt * H
-            nu[image] = np.fmax(temp_u[image], 0.5)
-            nu[~image] = np.fmin(temp_u[~image], 0.5)
+            nu[(image) & (A)] = np.fmax(temp_u[(image) & (A)], 0.5)
+            nu[(~image) & (A)] = np.fmin(temp_u[(~image) & (A)], 0.5)
 
+            avg_change = (1.0/np.sum(A) * np.sum((nu[A] - u[A])**2)) ** 0.5
+            print "avg", avg_change
+            #print n, "BinVol", np.sum(gx**2 + gy**2)
+            if avg_change <= THRESHOLD or n == N:
+                break
+
+            n += 1
             u = nu.copy()
+            del H
+            del temp_u
     else:
         raise NotImplemented
     return nu
@@ -79,16 +105,47 @@ def binvol(image):
     B = perim(image)
     return B
 
-def main():
+def __make_thorus():
+    x,y,z = ogrid[0:75,0:75,0:75]
+    c = 37 
+    r2 = 40
+    teta = pi/8.0
+    R_t = 25
+    r_t = 10
+    thorus = (R_t - sqrt((x-c)**2+(y-c)**2))**2 + (z-c)**2 <= r_t**2
+    return thorus
+
+def __test_thorus():
+    pass
+
+def __test_lena():
+    print "> Testing lena"
     from scipy.misc.common import lena
     from scipy.misc import imsave
     l = lena().astype('uint8')
     o = mahotas.otsu(l)
     #bl = np.empty(l.shape, dtype='uint8')
     bl = l >= o
-    s = smooth(bl) * 255
+    s = smooth(bl, N) 
+    print s.max(), s.min()
     imsave('smoothed_lena.png', s)
     imsave('lena.png', bl)
+
+def __test_circle():
+    print "> Testing circle"
+    from scipy.misc import imsave
+    y, x = np.ogrid[0: 500, 0: 500]
+    cx, cy = 250, 250
+    r = 200
+    ball = (y - cy)**2 + (x - cx)**2 <= r**2
+    smoothed_ball = smooth(ball, N)
+    imsave('ball.png', ball)
+    imsave('smoothed_ball.png', smoothed_ball)
+
+def main():
+    __test_lena()
+    __test_circle()
+    
 
 if __name__ == '__main__':
     main()
