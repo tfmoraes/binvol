7accaf389fceab89b0e29b097eaa23e07c6382ae
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-08-18 09:40:48 -0300

    Calculating gradient to 2d images

--- binvol_cy.pyx
+++ binvol_cy.pyx
@@ -14,8 +14,10 @@ def perim(np.ndarray image):
         return mahotas.labeled.borders(image, np.ones((3, 3, 3)))
 
 def smooth(np.ndarray image, int n):
-    cdef double dt
+    cdef double dt, gx, gy
+    cdef int i, j, k, p
     cdef np.ndarray A0, A1, A2, A3, A
+    cdef np.ndarray out
 
     if not image.ndim in (2, 3):
         raise ValueError("Only 2 and 3 dimensions are supported")
@@ -26,9 +28,25 @@ def smooth(np.ndarray image, int n):
     A3 = perim(A2)
     A = A0 | A1 | A2 | A3
 
+    out = image.astype('double')
+
     if image.ndim == 2:
-        pass
-    else image.ndim:
+        for p in xrange(n):
+            for i in xrange(image.shape[0]):
+                for j in xrange(image.shape[1]):
+                    if A[i, j]:
+                        if i + 1 < image.shape[0]:
+                            gx = out[i,j] - out[i+1, j]
+                        else:
+                            gx = image[i, j]
+
+                        if j + 1 < image.shape[1]:
+                            gy = out[i,j] - out[i, j + 1]
+                        else:
+                            gy = out[i, j]
+
+                        out[i, j] = ((gx * gx) + (gy * gy)) ** 0.5
+    else:
         pass
 
-    return A
+    return out
