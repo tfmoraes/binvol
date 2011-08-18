52957be5e261fcd741cb3bcb2376e8e4d0fec693
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-08-18 16:53:05 -0300

    calculationg H value

--- binvol_cy.pyx
+++ binvol_cy.pyx
@@ -25,7 +25,7 @@ cdef calculate_pixel_gradient_magnitude(np.ndarray image, int y, int x):
         if x + 1 < image.shape[1]:
             gx = image[y, x] - image[y, x + 1]
         else:
-            gy = image[y, x]
+            gx = image[y, x]
 
         gm = ((gx * gx) + (gy * gy)) ** 0.5
 
@@ -37,25 +37,28 @@ cdef calculate_pixel_mean_curvature(np.ndarray image, int x, int y):
     h = 1
     k = 1
     if image.ndim == 2:
-        fx = (image[y, x + h] - image[y, x - h]) / (2*h)
+        fx = (image[y, x + h] - image[y, x - h]) / (2.0*h)
 
-        fy = (image[y + k, x] - image[y - k, x]) / (2*k)
+        fy = (image[y + k, x] - image[y - k, x]) / (2.0*k)
 
         fxx = (image[y, x + h] - 2*image[y,x] + image[y, x - h]) / (h*h)
 
         fyy = (image[y + k, x] - 2*image[y,x] + image[y - k, x]) / (k*k)
 
-        fxy = (image[y + k, x + h] - image[y - k, x + h] - image[y + k, y - h] + image[y - k, x - h]) / (4*h*k)
+        fxy = (image[y + k, x + h] - image[y - k, x + h] - image[y + k, y - h] + image[y - k, x - h]) / (4.0*h*k)
 
-        curvature = (fxx*fy*fy - 2*fxy*fx*fy + fyy*fx*fx) / (fx*fx + fy*fy)**1.5
+        curvature = (fxx*(1 + fy*fy) - 2.0*fxy*fx*fy + fyy*(1+fx*fx)) /(2 * (1 + fx*fx + fy*fy)**1.5)
 
     return curvature
 
 def smooth(np.ndarray image, int n):
-    cdef double dt, gx, gy
+    cdef double dt, gm, K, H
     cdef int i, j, k, p
     cdef np.ndarray A0, A1, A2, A3, A
     cdef np.ndarray out
+    cdef np.ndarray tmp
+
+    dt = 1/6.0
 
     if not image.ndim in (2, 3):
         raise ValueError("Only 2 and 3 dimensions are supported")
@@ -70,10 +73,19 @@ def smooth(np.ndarray image, int n):
 
     if image.ndim == 2:
         for p in xrange(n):
+            tmp = out.copy()
             for i in xrange(image.shape[0]):
                 for j in xrange(image.shape[1]):
                     if A[i, j]:
-                        out[i, j] = calculate_pixel_gradient_magnitude(out, i, j)
+                        gm = calculate_pixel_gradient_magnitude(tmp, i, j)
+                        K = calculate_pixel_mean_curvature(tmp, i, j)
+                        H = gm * K
+                        print H
+                        if image[i, j]:
+                            out[i, j] = max((tmp[i,j] + dt*H, 0.5))
+                        else:
+                            out[i, j] = min((tmp[i,j] + dt*H, 0.5))
+
     else:
         pass
 
